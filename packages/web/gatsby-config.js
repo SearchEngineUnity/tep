1df56ca18103c4fc8d3f9c6c1b82cf54d7ca129a
sc-searchengineunity <sharon@searchengineunity.com>
2023-02-03 14:40:46 -0500

    add gatsby source sanity options

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -2,16 +2,25 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV || `development`}`,
 });
 
 const isProd = process.env.NODE_ENV === 'production';
 const previewEnabled = (process.env.GATSBY_IS_PREVIEW || 'false').toLowerCase() === 'true';
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://sitebuilderv1.netlify.app', // update to new netlify URL
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     title: `web`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl,
   },
   plugins: [
     {
