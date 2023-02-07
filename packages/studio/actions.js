31109a503a034b4bd438c40943b7f1d5873d88c1
sc-searchengineunity <sharon@searchengineunity.com>
2023-02-07 16:41:26 -0500

    adding gatsby preview action to page documents

---
+++ packages/studio/actions.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/prefer-default-export */
+import { RiGatsbyLine } from 'react-icons/ri';
+
+const contentSyncUrl = process.env.SANITY_STUDIO_CONTENT_SYNC_URL;
+
+export const GatsbyPreviewAction = ({ published, draft }) => {
+  const doc = draft || published;
+
+  if (!doc) return null;
+
+  let id;
+  if (published) {
+    id = published._id;
+  } else if (draft) {
+    // eslint-disable-next-line prefer-destructuring
+    id = draft._id.split('drafts.')[1];
+  }
+  const updatedAt = `${doc._updatedAt.split('.')[0].split('Z')[0]}.000Z`;
+
+  return {
+    disabled: !id,
+    icon: RiGatsbyLine,
+    label: 'Open Preview',
+    onHandle: () => {
+      window.open(
+        `${contentSyncUrl}/gatsby-source-sanity/${id}-${updatedAt}`,
+        `gatsby-source-sanity-${id}`,
+      );
+    },
+  };
+};
