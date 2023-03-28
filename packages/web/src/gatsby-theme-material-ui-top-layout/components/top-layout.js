6cf33508feecf59b804434c31bc097968e60003b
sc-searchengineunity <sharon@searchengineunity.com>
2023-03-28 15:43:06 -0400

    add responsive typography to base theme + fix blockquote

--- packages/web/src/gatsby-theme-material-ui-top-layout/components/top-layout.js
+++ packages/web/src/gatsby-theme-material-ui-top-layout/components/top-layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { deepmerge } from '@mui/utils';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
 import { useCustomTheme } from '../useCustomTheme';
 
 export default function TopLayout({ children, theme }) {
   const customTheme = useCustomTheme();
 
-  const mergedTheme = deepmerge(theme, customTheme);
+  let mergedTheme = deepmerge(theme, customTheme);
+  mergedTheme = responsiveFontSizes(mergedTheme);
 
   return (
     <ThemeProvider theme={mergedTheme}>
