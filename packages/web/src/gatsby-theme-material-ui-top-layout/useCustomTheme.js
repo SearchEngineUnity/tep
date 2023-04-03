e085e40efc20106ab22780462dfcbd592141c539
holly-searchengineunity <holly@searchengineunity.com>
2023-04-03 08:55:34 -0400

    changed MUI 5 breakpoints to MUI 4 at SB request

--- packages/web/src/gatsby-theme-material-ui-top-layout/useCustomTheme.js
+++ packages/web/src/gatsby-theme-material-ui-top-layout/useCustomTheme.js
@@ -364,6 +364,15 @@ export const useCustomTheme = () => {
   } = themeData;
 
   const customTheme = {
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+      },
+    },
     components: {
       // Style sheet name ⚛️
       MuiIcon: {
