5c3aceb1dd3fd8bf31b1c4c9d3f8ae219bf73567
holly-searchengineunity <holly@searchengineunity.com>
2023-04-27 11:48:07 -0400

    Fix product card symantic issues and tag issues

--- packages/web/src/components/portableText/insertable/productCard/ProductCard.js
+++ packages/web/src/components/portableText/insertable/productCard/ProductCard.js
@@ -90,83 +90,89 @@ function ProductCard({
           <Box sx={{ m: 3 }}>
             <Grid container spacing={2} alignItems="center">
               <Grid item xs={12} sm={4}>
-                <ButtonBase
-                  type="button"
-                  onClick={handleOpen}
-                  focusVisibleClassName={{}}
-                  sx={{
-                    border: 'none',
-                    background: 'none',
-                    height: 'auto',
-                    width: '100%',
-                    cursor: 'pointer',
-                  }}
-                >
-                  <GatsbyImage
-                    image={imageData}
-                    alt={image?.alt}
-                    style={{ display: 'block', maxWidth: '100%', maxHeight: '240px' }}
-                    objectFit="contain"
-                  />
-                  <Box
-                    component="span"
-                    sx={(theme) => ({
-                      position: 'absolute',
-                      left: 0,
-                      right: 0,
-                      top: 0,
-                      bottom: 0,
-                      bgcolor: 'common.black',
-                      opacity: 0,
-                      width: '100%',
-                      transition: theme.transitions.create('opacity'),
-                      color: 'white',
-                      display: 'inline-flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      '&:hover': {
-                        zIndex: 1,
-                        opacity: 0.4,
-                      },
-                    })}
-                  >
-                    <ZoomInIcon sx={{ fontSize: '120px' }} />
-                  </Box>
-                </ButtonBase>
-                {image.caption && <Caption blocks={image.caption} />}
-                <Modal
-                  open={open}
-                  onClose={handleClose}
-                  aria-labelledby="simple-modal-title"
-                  aria-describedby="simple-modal-description"
-                  sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-                >
-                  <Box
+                <Box component={Box} as="figure" sx={{ m: 0 }}>
+                  <ButtonBase
+                    type="button"
+                    onClick={handleOpen}
+                    focusVisibleClassName={{}}
                     sx={{
-                      display: 'flex',
-                      width: { xs: '80vw', md: '50vw' },
-                      height: { xs: '80vh', md: '50vh' },
-                      bgcolor: 'background.paper',
                       border: 'none',
-                      boxShadow: 5,
-                      p: 2,
-                      justifyContent: 'center',
+                      background: 'none',
+                      height: 'auto',
+                      width: '100%',
+                      cursor: 'pointer',
                     }}
                   >
                     <GatsbyImage
                       image={imageData}
                       alt={image?.alt}
+                      style={{ display: 'block', maxWidth: '100%', maxHeight: '240px' }}
                       objectFit="contain"
-                      style={{ display: 'block', maxWidth: '100%', height: 'auto' }}
                     />
-                  </Box>
-                </Modal>
-                <br />
-                <ButtonAffiliate {...mapMuiBtnToProps(topBtn)} />
-                <br />
+                    <Box
+                      sx={(theme) => ({
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0,
+                        bgcolor: 'common.black',
+                        opacity: 0,
+                        width: '100%',
+                        transition: theme.transitions.create('opacity'),
+                        color: 'white',
+                        display: 'inline-flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        '&:hover': {
+                          zIndex: 1,
+                          opacity: 0.4,
+                        },
+                      })}
+                    >
+                      <ZoomInIcon sx={{ fontSize: '120px' }} />
+                    </Box>
+                  </ButtonBase>
+
+                  {image.caption && (
+                    <Box component={Caption} as="figcaption" sx={{ textAlign: 'center' }}>
+                      <Caption blocks={image.caption} />
+                    </Box>
+                  )}
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                    sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                  >
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        width: { xs: '80vw', md: '50vw' },
+                        height: { xs: '80vh', md: '50vh' },
+                        bgcolor: 'background.paper',
+                        border: 'none',
+                        boxShadow: 5,
+                        p: 2,
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <GatsbyImage
+                        image={imageData}
+                        alt={image?.alt}
+                        objectFit="contain"
+                        style={{ display: 'block', maxWidth: '100%', height: 'auto' }}
+                      />
+                    </Box>
+                  </Modal>
+                </Box>
+                <Box sx={{ mt: 3 }}>
+                  <ButtonAffiliate {...mapMuiBtnToProps(topBtn)} />
+                </Box>
               </Grid>
               <Grid item xs={12} sm={8}>
-                <Typography component={headingLevel} variant="h4">
+                <Typography component={headingLevel || 'p'} variant="h4">
                   {name}
                 </Typography>
                 <ProductCardRating rating={rating} />
