9c3e2655c88da96841aa0b1c6748b3b54ee7b8a6
sc-searchengineunity <sharon@searchengeineunity.com>
2023-11-06 13:06:42 -0500

    adding fixed design TEP cta

--- packages/studio/schemas/sections/index.js
+++ packages/studio/schemas/sections/index.js
@@ -1,3 +1,4 @@
+export { default as cta } from './cta';
 export { default as paginatedListingSection } from './paginatedListingSection';
 export { default as lrFlex } from './lrFlex';
 export { default as lrHero } from './lrHero';
