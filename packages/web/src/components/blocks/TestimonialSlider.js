3db2ca755284f0901785ef31f311dad08a49b0b1
sc-searchengineunity <sharon@searchengeineunity.com>
2023-12-11 10:11:53 -0500

    remove uncessary imports from testimonial slider

--- packages/web/src/components/blocks/TestimonialSlider.js
+++ packages/web/src/components/blocks/TestimonialSlider.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import { IconButton } from '@mui/material';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
@@ -11,7 +8,7 @@ import Stack from '@mui/material/Stack';
 import CompanyTile from '../testimonialTiles/TestimonialSliderTile';
 import { mapTestimonialListToProps } from '../../lib/mapToProps';
 
-function TestimonialSlider({ testimonialList, layout, tileOption }) {
+function TestimonialSlider({ testimonialList }) {
   const [currentPage, setCurrentPage] = useState(0);
 
   const [slideDirection, setSlideDirection] = useState('left');
@@ -37,13 +34,14 @@ function TestimonialSlider({ testimonialList, layout, tileOption }) {
         alignContent: 'center',
         justifyContent: 'center',
         width: '100%',
-        py: '20px',
-        height: { lg: '280px', md: '400px', xs: 'auto' },
+        // height: { lg: '260px', md: '216px', sm: '330px', xs: '600px' },
       }}
     >
       <IconButton onClick={handlePrevPage} disabled={currentPage === 0}>
         {/* this is the button that will go to the previous page you can change these icons to whatever you wish */}
-        <NavigateBeforeIcon sx={{ width: '70px', height: '70px' }} />
+        <NavigateBeforeIcon
+          sx={{ width: { xs: '24px', md: '70px' }, height: { xs: '24px', md: '70px' } }}
+        />
       </IconButton>
       <Box sx={{ width: '100%', height: '100%' }}>
         {/* this is the box that holds the cards and the slide animation,
@@ -86,7 +84,9 @@ function TestimonialSlider({ testimonialList, layout, tileOption }) {
         onClick={handleNextPage}
         disabled={currentPage >= Math.ceil((testimonialList.length || 0) / cardsPerPage) - 1}
       >
-        <NavigateNextIcon sx={{ width: '70px', height: '70px' }} />
+        <NavigateNextIcon
+          sx={{ width: { xs: '24px', md: '70px' }, height: { xs: '24px', md: '70px' } }}
+        />
       </IconButton>
     </Box>
   );
