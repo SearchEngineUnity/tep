e7217321c03f5d6f594b9ed406c3d05af43498dc
sc-searchengineunity <sharon@searchengeineunity.com>
2023-12-15 15:31:59 -0500

    updating style to tile#9

--- packages/web/src/components/tiles/TileImageBackgroundTitle.js
+++ packages/web/src/components/tiles/TileImageBackgroundTitle.js
@@ -1,42 +1,50 @@
 // Tile #9
 import React from 'react';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import { Typography } from '@mui/material';
 import ConditionalCardActionArea from '../cardActionArea/ConditionalCardActionArea';
 
 function TileImageBackgroundTitle({ image, link, title }) {
   const bgImage = image.url;
 
   return (
-    <ConditionalCardActionArea link={link}>
-      <Box
-        sx={{
-          height: '380px',
-          width: '380px',
-          backgroundImage: { sx: 'none', sm: `url(${bgImage})` },
-          backgroundSize: 'cover',
-          backgroundPosition: 'center center',
-          backgroundRepeat: 'no-repeat',
-          borderRadius: '0px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
+    <Card square elevation={0}>
+      <ConditionalCardActionArea link={link}>
         <Box
           sx={{
-            border: '#406CCB solid 2px',
-            backgroundColor: 'white',
-            fontSize: '20px',
-            fontWeight: 700,
-            textAlign: 'center',
-            py: 2,
-            width: '280px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundImage: `url(${bgImage})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+            height: '380px',
+            width: '100%',
+            maxWidth: '380px',
+            px: '40px',
+            mx: 'auto',
           }}
         >
-          {title}
+          <Box
+            sx={{
+              border: '#406CCB solid 2px',
+              backgroundColor: 'white',
+              fontSize: 'body1.fontSize',
+              lineHeight: 'normal',
+              fontWeight: 700,
+              textAlign: 'center',
+              p: 2,
+              width: '100%',
+              wordBreak: 'break-all',
+            }}
+          >
+            {title}
+          </Box>
         </Box>
-      </Box>
-    </ConditionalCardActionArea>
+      </ConditionalCardActionArea>
+    </Card>
   );
 }
 
